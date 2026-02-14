3cdadd028859dd8fa79f439771691ab804f7f7a0
Lautaro Della Mea <lautadm.dev@gmail.com>
2026-02-14 16:44:37 -0300

    finish section 3

--- README.md
+++ README.md
@@ -32,6 +32,7 @@ venv\Scripts\activate
 
 # Linux/Mac
 source venv/bin/activate
+source venv/Scripts/activate
 ```
 
 4. Instala las dependencias:
@@ -67,3 +68,27 @@ La API estará disponible en: http://localhost:8000
 - FastAPI
 - Python 3.8+
 - Uvicorn
+
+# Sección 01
+
+## Temas de esta sección
+
+- ¿Qué es FastAPI y por qué aprenderlo?
+- Documentación y enlaces importantes
+- Instalación de FastAPI
+- Nuestro primer endpoint (GET)
+- Query Params
+- Path Params
+- Métodos HTTP y status code
+- Endpoint POST
+- Endpoint PUT
+- Endpoint DELETE
+- Documentación automática
+
+## Enlaces
+
+Durante las clases podrás encontrar algunos enlaces adjuntos, pero puedes consultarlos antes o usar este artículo como referencia para acceder a ellos más fácil:
+
+- [FastAPI — Documentación](https://fastapi.tiangolo.com/)
+- [Pydantic — Documentación](https://docs.pydantic.dev/latest/)
+- [JSON Schema — Sitio oficial](https://json-schema.org/)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Body, HTTPException
 
 app = FastAPI(title="Mini Blog", description="Esta es una API de ejemplo")
 
@@ -32,14 +32,14 @@ BLOG_POST = [
 
 @app.get("/")
 def home():
-  return {"message": "Bienvenido a la API de Mini Blog"}
+  return {"message": "Bienvenido a la API de Mini Blog de Lautaro"}
 
 
-# @app.get("/posts")
-# def list_posts():
-#   return {"data ": BLOG_POST}
+@app.get("/posts-general")
+def list_posts_general():
+  return {"data ": BLOG_POST}
 
-# Query params
+# Query params (define como queremos ese recurso, lo quiero traer filtrado, ordenado, etc)
 @app.get("/posts")
 def list_posts(query: str | None = Query(default=None, description="Texto para buscar en el titulo")):
   
@@ -59,7 +59,58 @@ def list_posts(query: str | None = Query(default=None, description="Texto para b
   return {"data ": BLOG_POST}
 
 
-# Path params
+# Path params (forma parte de la url directamente, trae un recurso especifico)
+# el include_content es un query param
+# El Path forma parte de la URL, mientras que el Query Param va después del ?
 @app.get("/posts/{id}")
-def get_post(id: int):
-  return {"data ": BLOG_POST[id]}
+def get_post(id: int, include_content: bool = Query(default=True, description="Si queremos traer el contenido del post")):
+  for post in BLOG_POST:
+    if post["id"] == id:
+      if include_content:
+        return {"data": post}
+      return {"data": {"id": post["id"], "title": post["title"]}}
+  
+  return {"message": "Post no encontrado"}
+
+
+# Metodo POST
+@app.post("/posts")
+def create_post(post: dict = Body(...)):
+  if "title" not in post or "content" not in post:
+    return {"error": "El post debe tener un titulo y un contenido"}
+  
+  if not str(post["title"]).strip() or not str(post["content"]).strip():
+    return {"error": "El titulo y contenido no pueden estar vacios"}
+  
+  new_id = (BLOG_POST[-1]["id"] + 1) if BLOG_POST else 1
+  new_post = {"id": new_id, "title": post["title"], "content": post["content"], "author": "Lautaro", "created_at": "2021-01-01", "updated_at": "2021-01-01"}
+  
+  BLOG_POST.append(new_post)
+  return {"message": "Post creado exitosamente", "data": new_post}
+
+
+# Metodo PUT
+@app.put("/posts/{post_id}")
+def update_post(post_id: int, data: dict = Body(...)): # los tres puntitos indican que es obligatorio mandar el body
+  for post in BLOG_POST:
+    if post["id"] == post_id:
+      if "title" in data:
+        post["title"] = data["title"]
+      if "content" in data:
+        post["content"] = data["content"]
+      post["updated_at"] = "2021-01-01"
+      return {"message": "Post actualizado exitosamente", "data": post}
+  
+    raise HTTPException(status_code=404, detail="Post no encontrado") # esto es para desatar un error personalizado
+    # raise → Interrumpe la ejecución y lanza un error.
+  
+  
+# Metodo DELETE
+@app.delete("/posts/{post_id}", status_code=204)
+def delete_post(post_id: int):
+  for index, post in enumerate(BLOG_POST):
+    if post["id"] == post_id:
+      BLOG_POST.pop(index)
+      return
+  
+  raise HTTPException(status_code=404, detail="Post no encontrado")
